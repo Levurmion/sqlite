9627d7073bb999dd3e834c11d975549b8dea278d
Elbert Timothy <elbert.timothy@touchstoneadvisory.co.uk>
2024-10-08 19:41:37 +0100

    implemented number of tables count

--- src/lib/dot-commands/dbinfo.cpp
+++ src/lib/dot-commands/dbinfo.cpp
@@ -3,6 +3,10 @@
 #include <fstream>
 
 void dbinfo (std::ifstream& databaseFile) {
+    // no conversions needed as std::cout will interpret chars as ASCII
+    char header[16];
+    databaseFile.read(header, 16);
+    std::cout << header << std::endl;
 
     // set file pointer to 16th byte
     databaseFile.seekg(16);
@@ -12,7 +16,21 @@ void dbinfo (std::ifstream& databaseFile) {
     databaseFile.read(buffer, 2);
 
     // big-endian: most sig byte in buffer[0], followed by buffer[1]
+    // static_cast to unsigned int will turn 0000 0001 to 0000 0000 0000 0001
+    // shift left by 8 bits turns this into 0000 0001 0000 0000
     unsigned int pageSize = (static_cast<unsigned int>(buffer[0]) << 8) | static_cast<unsigned int>(buffer[1]);
 
-    std::cout << "Page Size: " << pageSize << std::endl;
+    std::cout << "database page size:" << "\t" << pageSize << std::endl;
+
+    // B-tree page header is an 8 or 12-byte section at offset 0
+    // the first page includes the 100-byte databae file header so we will offset
+    // this by 100. The cell count for a page is at offset 3 from the start of
+    // the B-tree page.
+    databaseFile.seekg(100 + 3);
+
+    char cellCountBuffer[2];
+    databaseFile.read(cellCountBuffer, 2);
+    unsigned int cellCount = (static_cast<unsigned int>(cellCountBuffer[0]) << 8) | static_cast<unsigned int>(cellCountBuffer[1]);
+
+    std::cout << "number of tables:" << "\t" << cellCount << std::endl;
 }
